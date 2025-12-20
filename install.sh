eea234fc0bdd5d0bd0a85c4e03b655121809aa34
Erik Karlgren Domercq <erik.karldom@gmail.com>
2025-12-20 13:59:49 +0100

    Apply fixes to install.sh found by shellcheck
    Mostly quote all environment variables, since $HOME could be
    theoretically overriden and break this script

--- install.sh
+++ install.sh
@@ -12,24 +12,24 @@ fi
 
 echo "Installing"
 
-if ! install -C -D -t $HOME/.local/bin/ ./target/release/avis-imgv; then
+if ! install -C -D -t "$HOME/.local/bin/" ./target/release/avis-imgv; then
   echo "Installation failed -> exiting"
 fi
 
 echo "Installation complete"
 
-applications_dir=$HOME/.local/share/applications
+applications_dir="$HOME/.local/share/applications"
 
-mkdir -p $applications_dir
+mkdir -p "$applications_dir"
 
-if [ ! -f $applications_dir/avis-imgv.desktop ]; then
+if [ ! -f "$applications_dir/avis-imgv.desktop" ]; then
   echo "[Desktop Entry]
 Exec=$HOME/.local/bin/avis-imgv
 MimeType=image/png;image/jpeg;image/jpg;image/webp;
 Name=Avis Image Viewer
 NoDisplay=false
-Type=Application" >$applications_dir/avis-imgv.desktop
+Type=Application" > "$applications_dir/avis-imgv.desktop"
 
   echo "Updating desktop database"
-  update-desktop-database -v $applications_dir
+  update-desktop-database -v "$applications_dir"
 fi
